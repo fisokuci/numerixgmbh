b1c8ab3b1df671f2aa26eb895fb634324c590133
fisokuci <fisnik_k.09@hotmail.com>
2025-09-29 14:13:31 +0200

    Add stuff for building

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { createServer } from "./server";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: "./",
     host: "::",
     port: 8080,
     fs: {
